c486fc1c0a8939944f451f8c4e6e529927afb5e8
LastukhinaAnastasia <anastasia.lastukhina@yandex.ru>
2025-11-20 17:19:10 +0300

    Added some functions

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from PyQt6.QtCore import Qt, QRectF, QPointF
 from PyQt6.QtWidgets import QWidget, QApplication
 from random import randint
 from PyQt6 import uic
+from Ui import Ui_Form
 
 
-class Suprematism(QWidget):
+class Suprematism(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.do_paint = False
         self.x, self.y = -100, -100
@@ -31,7 +32,7 @@ class Suprematism(QWidget):
     def draw_flag(self, qp):
         size = randint(20, 100)
         self.x, self.y = randint(size, 400 - size), randint(size, 300 - size)
-        color = QColor(255, 255, randint(0,10))
+        color = QColor(randint(0,255), randint(0,255), randint(0,255))
         qp.setBrush(color)
         rect = QPointF(self.x, self.y)
         qp.drawEllipse(rect, size, size)
