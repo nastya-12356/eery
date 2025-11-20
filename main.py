516e17345741d581bf52dd5a8dff3cdcda27cd93
LastukhinaAnastasia <anastasia.lastukhina@yandex.ru>
2025-11-20 17:12:05 +0300

    Added some functions

--- main.py
+++ main.py
@@ -3,14 +3,46 @@ from PyQt6.QtGui import QPainter, QColor, QPolygonF
 from PyQt6.QtCore import Qt, QRectF, QPointF
 from PyQt6.QtWidgets import QWidget, QApplication
 from random import randint
-from math import sin, cos, pi
+from PyQt6 import uic
 
 
 class Suprematism(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.run)
         self.do_paint = False
         self.x, self.y = -100, -100
-        self.fig = 3
+        self.fig = 0
         self.setMouseTracking(True)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        size = randint(20, 100)
+        self.x, self.y = randint(size, 400 - size), randint(size, 300 - size)
+        color = QColor(255, 255, randint(0,10))
+        qp.setBrush(color)
+        rect = QPointF(self.x, self.y)
+        qp.drawEllipse(rect, size, size)
+
+
+    def run(self):
+        self.paint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec())
